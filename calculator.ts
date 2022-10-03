a2a28cae7163519873a168dfc57aade9b48d2a97
Leonado De la cruz <leonardodelacruz03@hotmail.com>
2022-10-03 15:06:51 -0500

    this part finish

--- calculator.ts
+++ calculator.ts
@@ -1,23 +1,29 @@
 // Crear un tipo de dato
 type Operation = 'multiply' | 'add' | 'subtract' | 'divide';
+type Result = number | undefined;
 
 const operations = [ 'multiply', 'add', 'subtract', 'divide' ];
 
 // Añadir los tipos que querramos o necesitemos 
 
-const calculator = (a: number, b: number, op: Operation) => {
+// Para asignar el tipo de valor que devuelve la función se le añade después de los parametros el datatype
+const calculator = (a: number, b: number, op: Operation): Result => {
     if (!operations.includes(op)) {
         console.log('This operation is undefined');
     }
 
-    if (op == 'add') return a + b;
-    if (op == 'subtract') return a - b;
-    if (op == 'multiply') return a * b;
-    if (op == 'divide') {
-        if (b == 0 || a == 0) return 'Can\t divide by zero, sorry';
+    if (op === 'add') return a + b;
+    if (op === 'subtract') return a - b;
+    if (op === 'multiply') return a * b;
+    if (op === 'divide') {
+        if (b === 0 || a === 0) throw new Error('Can\t divide by zero, sorry');
         return a / b;
     }
+
+    throw new Error('Operation is not valid');
 };
 
 const result = calculator(3, 6, 'add');
 console.log(result);
+
+// El process.argv permite recuperar los arguments que pasamos
--- multiplier.ts
+++ multiplier.ts
@@ -3,3 +3,36 @@ const multiplicator = (a:number, b:number, printText:string) => {
 };
 
 multiplicator(2, 4, "Multiplied numers 2 and 4, the result is: ");
+
+// Parsear argumentos
+const parseArguments = (args): Array<number> => {
+    if (args.length !== 4) throw new Error('Wrong number of arguments');
+
+    const firstNumber = Number(args[2]);
+    const secondNumber = Number(args[3]);
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        return [
+            firstNumber,
+            secondNumber
+        ]
+    } else {
+        throw new Error('Provided values were not numbers!!!')
+    }
+};
+
+const cleanArguments = parseArguments(process.argv);
+
+const a: number = Number(cleanArguments[0]); // Se está obligando a volverse un tipo número
+const b: number = Number(cleanArguments[1]); // Se está obligando a volverse un tipo número
+
+console.log(process.argv);
+/* [
+    'D:\\typescript-bootcamp\\node_modules\\ts-node\\dist\\bin.js',
+    'D:\\typescript-bootcamp\\multiplier.ts',
+    '3',
+    '4'
+] */
+
+// Esto lo devuelve el porcess.argv y por eso se cogieron las posiciones 2 y 3 
+
+multiplicator(a, b, `Multiplied ${ a } and ${ b }, and the result is: `)
