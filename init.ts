721185f3c82edf275713bede51e8774e2e713378
Leonado De la cruz <leonardodelacruz03@hotmail.com>
2022-10-03 12:26:15 -0500

    starting a project with ts ⭐

--- init.ts
+++ init.ts
@@ -1,1 +1,6 @@
 // Aprendiendo TypeScript
+let message:string = "Aprendiendo TypeScript";
+typeof message // "string"
+
+// Error message
+// message = 2  ### Lanza error ya que anteriormente se supo que la variable es string
